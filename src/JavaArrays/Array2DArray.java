6bfe68f31487d9fb4013473d80f3e8eaaabcda18
Oomkar Pandit Student <lolasoftware@gmail.com>
2018-11-28 10:16:05 +0000

    not planned save

---
+++ src/JavaArrays/Array2DArray.java
@@ -0,0 +1,28 @@
+package JavaArrays;
+
+import java.util.Scanner;
+
+public class Array2DArray {
+
+    public static void main(String[] args) {
+
+        // write your code here
+
+
+
+
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        int a[]=new int[n];
+        for (int i=0;i<n;i++){
+            a[i]=scan.nextInt();
+        }
+
+        scan.close();
+
+        // Prints each sequential element in array a
+        for (int i = 0; i < a.length; i++) {
+            System.out.println(a[i]);
+        }
+    }
+}
