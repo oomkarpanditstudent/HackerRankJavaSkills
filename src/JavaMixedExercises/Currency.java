9e0c246c067616d6b26e001ff2bb3c631e5de25f
Oomkar Pandit <45208579+oomkarpanditstudent@users.noreply.github.com>
2018-12-01 21:56:35 +0000

    solved currency number format exercise (#7)

--- src/JavaMixedExercises/Currency.java
+++ src/JavaMixedExercises/Currency.java
@@ -1,9 +1,33 @@
 package JavaMixedExercises;
+import java.util.*;
+import java.text.*;
+public class Currency {
 
-public class git sCurrency {
-/*
     public static void main(String[] args) {
 	// write your code here
+
+
+                double payment = 123456.789;
+                NumberFormat nf = NumberFormat.getCurrencyInstance(Locale.US);
+                String us=nf.format(payment);
+                nf = NumberFormat.getCurrencyInstance(Locale.CHINA);
+                String china=  nf.format(payment);
+                nf = NumberFormat.getCurrencyInstance(Locale.FRANCE);
+                String france=nf.format(payment);
+                Locale localeIndia=new Locale.Builder().setLanguage("en").setRegion("IN").build();
+                nf = NumberFormat.getCurrencyInstance(localeIndia);
+                String india=nf.format(payment);
+
+                System.out.println("US: " + us);
+                System.out.println("India: " + india);
+                System.out.println("China: " + china);
+                System.out.println("France: " + france);
+
+
     }
-*/
-}
+        }
+
+
+
+
+
