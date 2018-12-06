46ce62acd8845ec0b459cb439d0cd3c8fac4c3f9
Oomkar Pandit <45208579+oomkarpanditstudent@users.noreply.github.com>
2018-12-06 01:21:25 +0000

    Create BigInteger.java

---
+++ BigInteger.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String n = scanner.nextLine();
+    BigInteger bi1=new BigInteger(n);
+Boolean b1=bi1.isProbablePrime(1);
+        scanner.close();
+    if (b1==true){
+        System.out.println("prime");
+    }
+        else 
+       System.out.println("not prime");
+            
+        
+
+    }
+    
+    }
+
