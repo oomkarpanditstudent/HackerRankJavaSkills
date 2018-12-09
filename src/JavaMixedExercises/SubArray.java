58ddd51f858d8e1f4c740ef40bf61ed4fc54f42a
Oomkar Pandit <45208579+oomkarpanditstudent@users.noreply.github.com>
2018-12-09 04:24:38 +0000

    Java cleaning branches (#16)
    * solved the big integer exerise
    
    * adding further files to start on next assignment of subarray
    
    * copied a solution for subarray
    
    * solved exercises for Inheritance, Abstract classes etc
    
    * Revert "copied a solution for subarray"
    
    This reverts commit d045d0ba50ab1edabf35a8cc8fb78d830294173b.

--- src/JavaMixedExercises/SubArray.java
+++ src/JavaMixedExercises/SubArray.java
@@ -14,83 +14,22 @@ public class SubArray {
 
     int fullarray[]={1,-2,4,-5,1};
     int count=0;
-    int arraychild=0;
-    for (int i=0;i<fullarray.length;i++){
-       if (fullarray[i]<0){
-        System.out.println(fullarray[i]);
+    for (int num:fullarray){
+        if (num<0){
+        System.out.println(num);
         count++;
         }
     }
+        for (int sum:fullarray){
+
+            if (int num<0){
+                System.out.println(num);
+                count++;
+            }
+        }
         System.out.println(count);
 
-        int total=0;
-        for (int i=0;i<fullarray.length;i++){
-           total+=fullarray[i];
-             //  System.out.println("total for loop full for now in this run"+total);
-        }
-        if (total<0){
-            System.out.println("BEfore COUNT "+count);
-            System.out.println("BEfore Total "+total);
-            count++;
-            System.out.println("After COUNT "+count);
-        }
-        System.out.println(total);
-        System.out.println(count);
-         total=0;
-         int move=1;
-        for (int i=0;i<fullarray.length-move;i++){
-            total+=fullarray[i]+fullarray[i+move];
-           // System.out.println(total);
-           // System.out.println(count);
-            if (total<0){
-                System.out.println("Before COUNT "+count);
-                System.out.println("Before Total "+total);
-                count++;
-                System.out.println("After COUNT "+count);
-            total=0;
-            }
-            total=0;
 
-        }
-
-         System.out.println("Till now = "+count);
-        total=0;
-        int move2=2;
-        for (int i=0;i<fullarray.length-move2;i++){
-            total+=fullarray[i]+fullarray[i+move]+fullarray[i+move2];
-         //   System.out.println("loop "+i+total);
-
-            if (total<0){
-                System.out.println("Before COUNT "+count);
-                System.out.println("Before Total "+total);
-                count++;
-                System.out.println("After COUNT "+count);
-                total=0;
-
-            }
-            total=0;
-            // System.out.println(total);
-        }
-        total=0;
-      int  move3=3;
-        for (int i=0;i<fullarray.length-move3;i++){
-            total+=fullarray[i]+fullarray[i+move]+fullarray[i+move2]+fullarray[i+move3];
-            //   System.out.println("loop "+i+total);
-
-            if (total<0){
-                System.out.println("Before COUNT "+count);
-                System.out.println("Before Total "+total);
-                count++;
-                System.out.println("After COUNT "+count);
-                total=0;
-
-            }
-            total=0;
-            // System.out.println(total);
-        }
-
-        // System.out.println(total);
-        System.out.println("final count "+count);
     }
 
 }
